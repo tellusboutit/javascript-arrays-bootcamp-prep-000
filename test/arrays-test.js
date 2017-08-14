142c784394310232a5c966eb1f720c9fba925c54
Terrell Tullis <t.tullis34@outlook.com>
2017-08-14 15:49:15 +0000

    automatically backed up by learn

--- test/arrays-test.js
+++ test/arrays-test.js
@@ -1,5 +1,13 @@
+/*global describe, it */
+
+const expect = require('chai').expect
+const fs = require('fs')
+const jsdom = require('mocha-jsdom')
+const path = require('path')
+
 describe('arrays', () => {
   jsdom({
+    
     src: fs.readFileSync(path.resolve(__dirname, '..', 'arrays.js'), 'utf-8')
   })
 
